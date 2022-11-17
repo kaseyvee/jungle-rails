b3699d72737f4de6f0b4d6eedb236da435485881
kethnal <kaseyvaldez@hotmail.com>
2022-11-17 11:42:52 -0800

    Order category list by when created

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -3,7 +3,7 @@ class Admin::CategoriesController < ApplicationController
   http_basic_authenticate_with name: ENV['USERNAME'], password: ENV['PASSWORD']
 
   def index
-    @categories = Category.all
+    @categories = Category.order(id: :desc).all
   end
 
   def new
