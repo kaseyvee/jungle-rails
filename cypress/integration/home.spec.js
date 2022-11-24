e76842d0a98d7cd9ebcbecbb7a14598bf3e908cf
kethnal <kaseyvaldez@hotmail.com>
2022-11-23 17:53:48 -0800

    Make simple tests for viewing home page

---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,18 @@
+describe('Home Page', () => {
+  beforeEach(() => {
+    cy.visit('/')
+  });
+
+  it('displays homepage', () => {
+    cy.get('h2')
+      .should('have.text', 'Where you can find any plants!')
+  });
+
+  it("There are products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
+
+  it("There is 2 products on the page", () => {
+    cy.get(".products article").should("have.length", 2);
+  });
+})
